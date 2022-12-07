734a456dfa800c07e4d324774049d6cb4c1626b8
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2022-12-07 04:08:28 +0000

    User model created

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+import AuthRoles from "../utils/authRoles"
 
 const userschema = mongoose.Schema(
     {
@@ -7,8 +8,33 @@ const userschema = mongoose.Schema(
             required:[true,"Name is required"],
             maxLength:[50,"Name must be less than 50 characters"]
 
-        }
-        
+        },
+        email:{
+            type:String,
+            required:[true,"Email is required"],
+            unique:true
+            
+        },
+        password:{
+            type:String,
+            required:[true,"Password is required"],
+            minLength:[8,"Password must be atleat 8 character"],
+            select:false
+        },
+        role:{
+            type:String,
+            enum:Object.values(AuthRoles),
+            default:AuthRoles.USER
+        },
+        forgetPasswordToken:String,
+        forgetPasswordExpiry:Date,
+
+    },
+    {
+        timestamps:true
     }
+
 )
+export default mongoose.model("User",userschema);
+
 
