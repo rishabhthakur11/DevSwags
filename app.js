d82e9721cfeb315fe59654c65903dcc096cc1282
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2023-01-15 02:14:11 +0530

    imort fixed in app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-import express from "express";
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import morgan from "morgan"
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
-app.use(morgan('tiny')); // morgan loader
-
+app.use(morgan("tiny")); // morgan loader
+app.use("/", authRoutes);
 module.exports = app;
-
