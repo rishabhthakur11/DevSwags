39db80af4d19931c3f9f0cdebd2bf42bf1ee1c40
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2023-01-15 02:14:41 +0530

    changes in MongoDb connection syntax

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const mongoose = require("mongoose");
-const app = require("./app")
+const app = require("./app");
 const config = require("./config/index");
 
 // We are using self executing Function
-(async ()=>{
-    try {
-        await mongoose.connect(config.MONGO_URI);
+(async () => {
+  try {
+    await mongoose
+      .connect(config.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
         console.log("DB Connected Successfully");
+      })
+      .catch((error) => {
+        console.log("database connection failed. exiting now...");
+        console.error(error);
+        process.exit(1);
+      });
 
-        app.on("error", (error) => {
-            console.log("Error: " + error);
-            throw err;
-          });
-          app.listen(config.PORT, () => {
-            console.log(`Server Listening on: ${config.PORT}`);
-          });
-        
-    } catch (error) {
-        console.log("Error:" + error);
-        throw error
-    }
-})()
+    app.on("error", (error) => {
+      console.log("Error: " + error);
+      throw error;
+    });
+    app.listen(config.PORT, () => {
+      console.log(`Server Listening on: ${config.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error:" + error);
+    throw error;
+  }
+})();
