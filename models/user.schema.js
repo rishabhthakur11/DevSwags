ca3474fde408d35decd410d6fb0e967f809d600e
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2023-01-15 00:29:00 +0530

    import and export typo fixed

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,89 +1,81 @@
 const mongoose = require("mongoose");
-import AuthRoles from "../utils/authRoles"
-import bcryrt from "bcryptjs"
-import JWT from  "jsonwebtoken"
-import config from "../config";
-import crypto from "crypto";
-
-
+const AuthRoles = require("../utils/authRoles");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const config = require("../config/index");
 
 const userschema = mongoose.Schema(
-    {
-        name:{
-            type:String,
-            required:[true,"Name is required"],
-            maxLength:[50,"Name must be less than 50 characters"]
-
-        },
-        email:{
-            type:String,
-            required:[true,"Email is required"],
-            unique:true
-            
-        },
-        password:{
-            type:String,
-            required:[true,"Password is required"],
-            minLength:[8,"Password must be atleat 8 character"],
-            select:false
-        },
-        role:{
-            type:String,
-            enum:Object.values(AuthRoles),
-            default:AuthRoles.USER
-        },
-        forgetPasswordToken:String,
-        forgetPasswordExpiry:Date,
-
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      maxLength: [50, "Name must be less than 50 characters"],
     },
-    {
-        timestamps:true
-    }
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be atleat 8 character"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
+    },
+    forgetPasswordToken: String,
+    forgetPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-)
+// @encrypt the user password
 
-// @encrypt the user password 
-
-userschema.pre("save",async function(next){
-    if(!this.modified("password")) return next();
-    this.password = await bcryrt.hash(this.password,10);
-    next();
-
-})
+userschema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = await bcryrt.hash(this.password, 10);
+  next();
+});
 
 // Adding more features to your schema
-userschema.methods={
-    // compare the password
-    comparePassword: async function(enteredPassword){
-        return await bcryrt.compare(enteredPassword,this.password);
-    },
-   // generate the JWT token
-   getJwtToken: function(){
+userschema.methods = {
+  // compare the password
+  comparePassword: async function (enteredPassword) {
+    return await bcryrt.compare(enteredPassword, this.password);
+  },
+  // generate the JWT token
+  getJwtToken: function () {
     return JWT.sign(
-        {
-            _id:this._id,
-            role:this.role
-        },
-        config.JWT_SECRET,
-        {
-            expiresIn: config.JWT_EXPIRY
-
-        }
-
-    )
-   },
-   generateForgetPasswordToken: function(){
-    const forgotToken = crypto.randomBytes(20).toString('hex');
+      {
+        _id: this._id,
+        role: this.role,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: config.JWT_EXPIRY,
+      }
+    );
+  },
+  generateForgetPasswordToken: function () {
+    const forgotToken = crypto.randomBytes(20).toString("hex");
 
     // save to DB
-    this.forgetPasswordToken = crypto.createHash("sha256").update(forgotToken).digest("hex");
+    this.forgetPasswordToken = crypto
+      .createHash("sha256")
+      .update(forgotToken)
+      .digest("hex");
     this.forgetPasswordExpiry = Date.now() + 20 * 60 * 1000;
 
-    // return the value to user 
+    // return the value to user
     return forgotToken;
-   } 
-}
+  },
+};
 
-export default mongoose.model("user",userschema);
-
-
+module.exports = mongoose.model("user", userschema);
