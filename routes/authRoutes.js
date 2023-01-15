af65c1412a3583005ff714a974fad602d9837b62
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2023-01-15 23:57:32 +0530

    logout route added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const login = require("../controllers/authControllers/login.controller");
+const logout = require("../controllers/authControllers/logout.controller");
 const signup = require("../controllers/authControllers/signup.controller");
 
 const router = express.Router();
 
 router.post("/api/auth/signup", signup);
 router.post("/api/auth/login", login);
+router.post("/api/auth/logout", logout);
 
 module.exports = router;
