15b438747773db7f5ec3ebc4687f45e7a2c4c93d
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2023-01-15 21:01:48 +0530

    Added login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const login = require("../controllers/authControllers/login.controller");
 const signup = require("../controllers/authControllers/signup.controller");
 
 const router = express.Router();
 
 router.post("/api/auth/signup", signup);
+router.post("/api/auth/login", login);
 
 module.exports = router;
